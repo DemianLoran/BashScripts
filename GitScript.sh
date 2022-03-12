f34cf46f363586c6aa056460c35bc1dfc24c6722
DemianLoran <demian_al_87@hotmail.com>
2022-03-12 18:03:40 +0100

    $argumento1

--- GitScript.sh
+++ GitScript.sh
@@ -33,6 +33,10 @@ function helpPanel(){
     #git add nombre_del_archivo, agrega un archivo al repositorio
     echo -e "\n\t${purpleColour}[-a][..]${endColour}\t${yellowColour}Agrega un archivo al repositorio ${endColour}${grayColour}Nombre-del-archivo${endColour}"
     #git status, nos permite ver el estado de todos nuestros archivos y carpetas.
+    echo -e "\n\t${purpleColour}[-st]${endColour}\t\t${yellowColour}Git status directorio actual${endColour}" 
+    #Commit con las modificaciones realizadas en el repositorio.
+    echo -e "\n\t${purpleColour}[-c][..]${endColour}\t${yellowColour}Commit en el repositorio local ${endColour}${grayColour}Nombre-del-archivo${endColour}"
+
     #git rm --cached: Elimina los archivos de nuestro repositorio local y del área de staging, pero los mantiene en nuestro disco duro
     #git rm --force: Elimina los archivos de Git y del disco duro.
     #git checkout + ID del commit nos permite viajar en el tiempo. Podemos volver a cualquier versión anterior de un archivo específico o incluso del proyecto entero.
@@ -53,14 +57,7 @@ function helpPanel(){
     echo -e "\n\t${purpleColour}[-r][..]${endColour}\t${yellowColour}Borra un tag del repositorio remoto con ${endColour}${grayColour}Nombre-del-tag${endColour}"    
     #Acceso a Panel de ayuda
     #echo -e "\n\t${purpleColour}[-h]${endColour}\t\t${yellowColour}Help Panel ${endColour}"
-
-    #Crear un nuevo tag y asignarlo a un commit: git tag -a nombre-del-tag id-del-commit.
-    #Borrar un tag en el repositorio local: git tag -d nombre-del-tag.
-    #Listar los tags de nuestro repositorio local: git tag o git show-ref --tags.
-    #Publicar un tag en el repositorio remoto: git push origin --tags.
-    #Borrar un tag del repositorio remoto: git tag -d nombre-del-tag y git push origin :refs/tags/nombre-del-tag.
-    #git rebase reescribe la historia del repositorio, cambia la historia de donde comenzó la rama y solo debe ser usado de manera local.
-
+    
     #FUNCION DE SALIDA CORRECTA
     exitt
 
@@ -70,33 +67,13 @@ function helpPanel(){
     #git clean --dry-run .. Para saber qué archivos vamos a borrar tecleamos
     #git clean -f .. Para borrar todos los archivos listados (que no son carpetas) tecleamos
     #git cherry-pick IDCommit .. Existe un mundo alternativo en el cual vamos avanzando en una rama pero necesitamos en master uno de esos avances de la rama, para eso utilizamos el comando. 
-    #git commit --amend ..  lo que hará es que los cambios que hicimos nos los agregará al commit anterior.
+   
     #git reset --soft HashDelHEAD te mantiene lo que tengas en staging ahí.
     #git reset --hard HashDelHEAD resetea absolutamente todo incluyendo lo que tengas en staging.
     #Crear una rama en el repositorio local: git branch nombre-de-la-rama o git checkout -b nombre-de-la-rama.
     #Publicar una rama local al repositorio remoto: git push origin nombre-de-la-rama.
     #Git clone +url-publica ->para descargar un duplicado del repositorio.
     #git merge +rama, merge a la rama indicada desde la que  estas ahora.
-   
-    
-    #git grep color -->use la palabra color
-    #git grep la --> donde use la palabra la
-    #git grep -n color–> en que lineas use la palabra color
-    #git grep -n platzi --> en que lineas use la palabra platzi  
-    #git grep -c la --> cuantas veces use la palabra la
-    #git grep -c paltzi --> cuantas veces use la palabra platzi
-    #git grep -c “<p>”–> cuantas veces use la etiqueta <p>
-    #git log -S “cabecera” --> cuantas veces use la palabra cabecera en todos los commits.
-    #grep–> para los archivos
-    #log --> para los commits.
-    #git shortlog -sn = muestra cuantos commit han hecho cada miembros del equipo.
-    #git shortlog -sn --all = muestra cuantos commit han hecho cada miembros del equipo hasta los que han sido eliminado
-    #git shortlog -sn --all --no-merge = muestra cuantos commit han hecho cada miembros quitando los eliminados sin los merges
-    #git blame ARCHIVO = muestra quien hizo cada cosa linea por linea
-    #git COMANDO --help = muestra como funciona el comando.
-    #git blame ARCHIVO -Llinea_inicial,linea_final= muestra quien hizo cada cosa linea por linea indicándole desde que linea ver ejemplo -L35,50
-    #git branch -r **= se muestran todas las ramas remotas
-    #git branch -a = se muestran todas las ramas tanto locales como remotas
 }
 
 function exitt(){
@@ -124,6 +101,18 @@ function git_Log(){
 function git_Add(){
     clear
     git add $argumento1
+    exitt
+}
+
+function git_Status(){
+    clear
+    git status
+    exitt
+}
+
+function git_Commit(){
+    clear
+    git commit -m '$argumento1'
     exitt
 }
 
@@ -158,12 +147,14 @@ function borra_Tag_Remoto(){
 }
 
 #PROCESO PRINCIPAL
-declare -i parameter_counter=0; while getopts 'islatdvpr' opt; do
+declare -i parameter_counter=0; while getopts 'islastctdvpr' opt; do
     case $opt in
         i) git_Init;;
         s) git_Show;;
         l) git_Log;;
         a) argumento1=$2; git_Add $argumento1;;
+        st) git_Status;;
+        c) argumento1=$2; git_Commit $argumento1;;
         t) argumento1=$2; argumento2=$3; nuevo_Tag $argumento1 $argumento2;;
         d) argumento1=$2; borra_Tag $argumento1;;
         v) lista_Tag;;
